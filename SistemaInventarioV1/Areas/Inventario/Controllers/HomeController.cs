6feced7f8dad880b208b6d3af36f3ea218f51b97
pedromcttj <pedro.mendoza@tectijuana.edu.mx>
2026-06-26 09:57:26 -0700

    Diseno Pagina Principal

--- SistemaInventarioV1/Areas/Inventario/Controllers/HomeController.cs
+++ SistemaInventarioV1/Areas/Inventario/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using SistemaInventarioAccesoDatos.Repositorio.IRepositorio;
+using SistemaInventarioModelos;
 using SistemaInventarioModelos.ErrorViewModel;
 using SistemaInventarioModelos.ViewModels;
 using System.Diagnostics;
@@ -8,10 +10,19 @@ namespace SistemaInventarioV1.Areas.Inventario.Controllers
     [Area("Inventario")]
     public class HomeController : Controller
     {
-       
-        public IActionResult Index()
-        {
-            return View();
+        private readonly ILogger<HomeController> _logger;
+        private readonly IunidadTrabajo _unidadTrabajo;
+
+        public HomeController(ILogger<HomeController> logger, IunidadTrabajo unidadTrabajo) { 
+         
+            _logger = logger;
+            _unidadTrabajo = unidadTrabajo;
+        }
+        
+        public async Task<IActionResult> Index()
+        { 
+            IEnumerable<Producto> productoLista = await _unidadTrabajo.Producto.ObtenerTodos();
+            return View(productoLista);
         }
         
         public IActionResult Privacy()
